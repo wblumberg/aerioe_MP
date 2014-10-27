a3d2b108413f4cc5a5a1196d17c2b078c820ceb1
Greg Blumberg <wblumberg@ou.edu>
2014-10-27 13:47:24 -0500

    Updated paths to .sav file and removed some debug statements.

--- spawn_idl.py
+++ spawn_idl.py
@@ -1,4 +1,3 @@
-#import pidly
 import subprocess
 import sys
 
@@ -8,16 +7,11 @@ prior = sys.argv[3]
 bhour = sys.argv[4]
 ehour = sys.argv[5]
 
-save_file = 'aerioe.idl_version_8p1.date_compiled_20140325.Release_1_4.sav'
-
-#idl = pidly.IDL()
+save_file = "/Users/dturner/vip/src/aerioe/aerioe.idl_version_8p1.date_compiled_20141027.Release_1_5.sav"
 string = 'set_plot,\'z\''
 #Turn off X-window plotting
-#idl('set_plot,\"z\"')
 
 #Restore the save file with the procedures and variables
-print 'restore,\'' + save_file + "\'"
-#idl('restore,\'' + save_file + "\'")
 string = string + '&' + 'restore,\'' + save_file + "\'"
 string = string + '&' + 'aerioe,' + str(date) + ',\'' + str(vip) + '\',\'' + prior + '\',shour=' + str(bhour) + ',ehour=' + str(ehour)
 
