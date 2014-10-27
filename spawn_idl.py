cae46a9d236de9ccf34541b7e189bd5fa13a2999
Greg Blumberg <wblumberg@ou.edu>
2014-10-27 13:45:13 -0500

    Removed dependency on pidly.py for spawning IDL processes.

--- spawn_idl.py
+++ spawn_idl.py
@@ -1,4 +1,5 @@
-import pidly
+#import pidly
+import subprocess
 import sys
 
 date = sys.argv[1]
@@ -9,16 +10,19 @@ ehour = sys.argv[5]
 
 save_file = 'aerioe.idl_version_8p1.date_compiled_20140325.Release_1_4.sav'
 
-idl = pidly.IDL()
-
+#idl = pidly.IDL()
+string = 'set_plot,\'z\''
 #Turn off X-window plotting
-idl('set_plot,\"z\"')
+#idl('set_plot,\"z\"')
 
 #Restore the save file with the procedures and variables
 print 'restore,\'' + save_file + "\'"
-idl('restore,\'' + save_file + "\'")
-#Run the AERIoe procedure
-print 'aerioe,' + str(date) + ',\'' + str(vip) + '\',\'' + prior + '\',shour=' + str(bhour) + ',ehour=' + str(ehour)
-idl('aerioe,' + str(date) + ',\'' + str(vip) + '\',\'' + prior + '\',shour=' + str(bhour) + ',ehour=' + str(ehour))
-idl.close()
- 
+#idl('restore,\'' + save_file + "\'")
+string = string + '&' + 'restore,\'' + save_file + "\'"
+string = string + '&' + 'aerioe,' + str(date) + ',\'' + str(vip) + '\',\'' + prior + '\',shour=' + str(bhour) + ',ehour=' + str(ehour)
+
+print string
+
+print 'idl -e \"' + string + '\"'
+p = subprocess.Popen('idl -e \"' + string + '\"', shell=True)
+p.wait()
